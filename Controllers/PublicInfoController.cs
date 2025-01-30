44316360b711f0b8a9273d9b1a2ba937bb0d7c50
anetrrr <anetor.ataga@gmail.com>
2025-01-30 19:13:59 +0100

    Add project files.

---
+++ Controllers/PublicInfoController.cs
@@ -0,0 +1,35 @@
+﻿using HNGStage0.Model.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace HNGStage0.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublicInfoController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetInfo()
+        {
+            var details = new PublicInfo
+            {
+                Email = "anetor.ataga@gmail.com",
+                CurrentDateTime = DateTime.Now,
+                GithubUrl = "www.github.com/anetrr/hngstage0",
+
+            };
+
+            if (details != null)
+            {
+
+                return Ok(details);
+
+            }
+
+            else return StatusCode((int)HttpStatusCode.BadRequest);
+                    
+        }
+    }
+}
---
+++ Model/Domain/PublicInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace HNGStage0.Model.Domain
+{
+    public class PublicInfo
+    {
+        public string Email { get; set; }
+        public DateTime CurrentDateTime { get; set; } = DateTime.Now;
+
+        public string GithubUrl { get; set; }
+    }
+}
